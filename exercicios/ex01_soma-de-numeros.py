fb801e685a4b6ab53e922dd60ce8657a7ab19b67
eusoufel <imperioruizbertoli@gmail.com>
2024-09-27 19:15:30 -0300

    Create ex01_soma-de-numeros.py
    soma de 2 números

---
+++ exercicios/ex01_soma-de-numeros.py
@@ -0,0 +1,6 @@
+# faça a soma de dois números 
+
+n1 = int(input('Digite o primeiro número :'))
+n2 = int(input('Digite o segundo número :'))
+
+print('A soma dos números {} + {} é {}'.format(n1, n2, (n1+n2)))
