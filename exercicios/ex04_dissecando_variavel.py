a3751c2986dc1a903385b15c61c90601c989dd93
eusoufel <imperioruizbertoli@gmail.com>
2024-09-28 20:04:53 -0300

    Update ex04_dissecando_variavel.py
    ex02

--- exercicios/ex04_dissecando_variavel.py
+++ exercicios/ex04_dissecando_variavel.py
@@ -1,13 +1,12 @@
 # Dissecando uma variável 
 
-s = input('Digite algo: ')
+s = input('Digite algo? ')
 
-print('O tipo primitivo desse valor é {} '.format(type(s)))
-
+print('O tipo primitivo desse valor é {}'.format(type(s)))
 print('Só tem espaços? {}'.format(s.isspace()))
-print('É um número? {}'.format(s.isalnum()))
-print('É alfbético? {}'.format(s.isalpha()))
-print('É alfanúmerico? {}'.format(s.isalnum()))
-print('Está em maiúscula? {}'.format(s.isupper()))
-print('Está em minúscula? {}'.format(s.islower()))
-print('Está Capitalizada? {}'.format(s.istitle()))
+print('É um número? {}'.format(s.isnumeric()))
+print('É alfabético? {}'.format(s.isalpha()))
+print('É alfanumérico? {}'.format(s.isalnum()))
+print('Está em maiúsculo? {}'.format(s.isupper()))
+print('Está em minúsculo? {}'.format(s.islower()))
+print('Está capitalizada? {}' .format(s.istitle()))
