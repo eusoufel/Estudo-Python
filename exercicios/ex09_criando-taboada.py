755f1cbbea0a90dd8b5df79a78be6026ac420b1c
eusoufel <imperioruizbertoli@gmail.com>
2024-10-02 19:39:25 -0300

    alt

---
+++ exercicios/ex09_criando-taboada.py
@@ -0,0 +1,15 @@
+# VAMOS CRIAR UMA TABOADA
+
+
+t = int(input('Qual taboa você deseja? '))
+
+print('{} x {} = {}'.format(1,t,1*t))
+print('{} x {} = {}'.format(2,t,2*t))
+print('{} x {} = {}'.format(3,t,3*t))
+print('{} x {} = {}'.format(4,t,4*t))
+print('{} x {} = {}'.format(5,t,5*t))
+print('{} x {} = {}'.format(6,t,6*t))
+print('{} x {} = {}'.format(7,t,7*t))
+print('{} x {} = {}'.format(8,t,8*t))
+print('{} x {} = {}'.format(9,t,9*t))
+print('{} x {} = {}'.format(10,t,10*t))
