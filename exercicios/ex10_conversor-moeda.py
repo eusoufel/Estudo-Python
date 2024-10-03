499c9903780e3d66bd9fdcd337ded901af370e3a
eusoufel <imperioruizbertoli@gmail.com>
2024-10-03 19:10:44 -0300

    ex11

--- exercicios/ex10_conversor-moeda.py
+++ exercicios/ex10_conversor-moeda.py
@@ -1,5 +1,5 @@
 # CONVERSOR DE MOEDA PARA DOLLAR 
 
-d = float(input('Quanto dinheiro você tem na carteira? '))
+d = float(input('Quanto dinheiro você tem na carteira :'))
 
-print('Com R$:{:.2f} você pode comprar U$:{:.2f}'.format(d,d * 5.48))
+print('Com R${:.2f} você pode comprar U${:.2f}'.format(d,d * 5.48))
